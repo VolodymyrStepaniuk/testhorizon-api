0fd52746aaf3094e5cc265e4361b5216ac728dc3
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-12-01 13:11:34 +0200

    Init of project

---
+++ src/main/java/com/stepaniuk/testhorizon/TesthorizonApplication.java
@@ -0,0 +1,13 @@
+package com.stepaniuk.testhorizon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesthorizonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesthorizonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stepaniuk/testhorizon/TesthorizonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepaniuk.testhorizon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesthorizonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
