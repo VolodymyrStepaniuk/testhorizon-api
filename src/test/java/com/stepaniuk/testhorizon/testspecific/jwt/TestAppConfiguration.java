15168280298d12ab916a8ab7ae98df255a7190ce
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2025-01-30 22:54:32 +0200

    Update Gradle workflow and remove JwtProvider bean from TestAppConfiguration

--- src/test/java/com/stepaniuk/testhorizon/testspecific/jwt/TestAppConfiguration.java
+++ src/test/java/com/stepaniuk/testhorizon/testspecific/jwt/TestAppConfiguration.java
@@ -1,6 +1,5 @@
 package com.stepaniuk.testhorizon.testspecific.jwt;
 
-import com.stepaniuk.testhorizon.security.JwtProvider;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -8,11 +7,6 @@ import org.springframework.security.authentication.TestingAuthenticationProvider
 
 @TestConfiguration
 public class TestAppConfiguration {
-
-    @Bean
-    public JwtProvider jwtProvider() {
-        return new JwtProvider();
-    }
 
     @Bean
     AuthenticationProvider authenticationProvider(){
