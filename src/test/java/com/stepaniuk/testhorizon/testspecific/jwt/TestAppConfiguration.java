aafe7ff77596c6a9d953814ab110154854401453
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2025-01-30 22:43:18 +0200

    Added JwtProvider bean to TestAppConfiguration

--- src/test/java/com/stepaniuk/testhorizon/testspecific/jwt/TestAppConfiguration.java
+++ src/test/java/com/stepaniuk/testhorizon/testspecific/jwt/TestAppConfiguration.java
@@ -1,5 +1,6 @@
 package com.stepaniuk.testhorizon.testspecific.jwt;
 
+import com.stepaniuk.testhorizon.security.JwtProvider;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -7,6 +8,11 @@ import org.springframework.security.authentication.TestingAuthenticationProvider
 
 @TestConfiguration
 public class TestAppConfiguration {
+
+    @Bean
+    public JwtProvider jwtProvider() {
+        return new JwtProvider();
+    }
 
     @Bean
     AuthenticationProvider authenticationProvider(){
